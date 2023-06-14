208d119e3000571b7a2c84bf5f55cb0289593f75
yahirdev13 <yahir.dev13@gmail.com>
2023-06-14 17:54:54 -0600

    Cambios en la home del cliente

---
+++ src/common/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../common/FooterStyle.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div class="pie-pagina">
+
+                <div class="grupo-2">
+                    <small>&copy; 2023 <b>Jardines de México</b> - Todos los Derechos Reservados.</small>
+                </div>
+            </div>
+        </footer>
+    )
+}
