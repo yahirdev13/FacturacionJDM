0d48ed2791bb4fc1e4fc950a7697d9be012dfbfc
yahirdev13 <yahir.dev13@gmail.com>
2023-06-26 10:25:32 -0600

    cambios a contactanos admin, tabla

--- src/screens/admin/ContactScreen.js
+++ src/screens/admin/ContactScreen.js
@@ -27,43 +27,53 @@ export default function ContactScreen() {
     <div>
       <NavAdmin />
       <MenuAdmin />
-      <div style={{ paddingLeft: "8%", paddingRight: "5%" }}>
-        <h1 class="mt-5">Control de contactos</h1>
-        <div style={{ left: "250px" }} className='container-fluid'>
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Teléfono</th>
-                <th scope="col">Correo Electrónico</th>
-                <th scope="col">Acciones</th>
-              </tr>
-            </thead>
-            <tbody class="table-group-divider">
-              <tr style={{ verticalAlign: "middle" }}>
-                <th scope="row ">1</th>
-                <td>Yahir Alberto Diaz Gonzalez</td>
-                <td>7271083065</td>
-                <td>yahird59@gmail.com</td>
-                <td>
-                  <button type="button" class="btn btn-primary " style={{ marginRight: "10px" }} data-bs-toggle="modal" data-bs-target="#detallesContacto">Detalles</button>
-                  <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#eliminarContacto">Eliminar</button>
-                </td>
-              </tr>
-              <tr style={{ verticalAlign: "middle" }}>
-                <th scope="row ">2</th>
-                <td>Misael Bahena Diaz</td>
-                <td>7773647629</td>
-                <td>misaelbd@gmail.com</td>
-                <td>
-                  <button type="button" class="btn btn-primary " style={{ marginRight: "10px" }}>Detalles</button>
-                  <button type="button" class="btn btn-danger ">Eliminar</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div style={{ paddingLeft: "8%", paddingRight: "5%", paddingTop: "3%" }}>
+        <div class="card">
+          <div class="card-header">
+            <h1 class="">Control de contactos</h1>
+          </div>
+          <div class="card-body">
+            <blockquote class="blockquote mb-0">
+              <div style={{ left: "250px" }} className='container-fluid'>
+                <table class="table ">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Nombre</th>
+                      <th scope="col">Teléfono</th>
+                      <th scope="col">Correo Electrónico</th>
+                      <th scope="col">Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody class="table-group-divider">
+                    <tr style={{ verticalAlign: "middle" }}>
+                      <th scope="row ">1</th>
+                      <td>Yahir Alberto Diaz Gonzalez</td>
+                      <td>7271083065</td>
+                      <td>yahird59@gmail.com</td>
+                      <td>
+                        <button type="button" class="btn btn-primary " style={{ marginRight: "10px" }} data-bs-toggle="modal" data-bs-target="#detallesContacto">Detalles</button>
+                        <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#eliminarContacto">Eliminar</button>
+                      </td>
+                    </tr>
+                    <tr style={{ verticalAlign: "middle" }}>
+                      <th scope="row ">2</th>
+                      <td>Misael Bahena Diaz</td>
+                      <td>7773647629</td>
+                      <td>misaelbd@gmail.com</td>
+                      <td>
+                        <button type="button" class="btn btn-primary " style={{ marginRight: "10px" }}>Detalles</button>
+                        <button type="button" class="btn btn-danger ">Eliminar</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </blockquote>
+          </div>
         </div>
+
+
       </div>
 
       <div class="toast-container position-fixed bottom-0 end-0 p-3">
