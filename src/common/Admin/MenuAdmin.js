beb5e3c39509a2f9b4d1a8f852fca0a4490e0ff0
yahirdev13 <yahir.dev13@gmail.com>
2023-06-26 09:53:28 -0600

    cambios en el menu del admin

--- src/common/Admin/MenuAdmin.js
+++ src/common/Admin/MenuAdmin.js
@@ -14,12 +14,11 @@ export default function MenuAdmin() {
 
     const navigate = useNavigate();
     const [selectedNavItem, setSelectedNavItem] = useState('profile');
-
     const handleNavItemSelect = (selected) => {
         console.log(selected);
         setSelectedNavItem(selected);
         navigate('/' + selected);
-        
+
     };
 
     return (
@@ -27,38 +26,38 @@ export default function MenuAdmin() {
             <SideNav
                 style={{ backgroundColor: "#2c497f", paddingTop: "4%" }}
                 onSelect={handleNavItemSelect}
-                >
+            >
 
-                
-                    <SideNav.Toggle />
-                    <SideNav.Nav defaultSelected={selectedNavItem}>
 
-                        <NavItem eventKey="profile">
-                            <NavIcon><i style={{ fontSize: "1.5em" }}><BsPersonFill /></i></NavIcon>
-                            <NavText style={{ fontSize: "20px" }}>Perfil</NavText>
-                        </NavItem>
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected={selectedNavItem}>
 
-                        <NavItem eventKey="users">
-                            <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillPeopleFill /></i></NavIcon>
-                            <NavText style={{ fontSize: "20px" }}>Usuarios</NavText>
-                        </NavItem>
+                    <NavItem eventKey="profile">
+                        <NavIcon><i style={{ fontSize: "1.5em" }}><BsPersonFill /></i></NavIcon>
+                        <NavText style={{ fontSize: "20px" }}>Perfil</NavText>
+                    </NavItem>
 
-                        <NavItem eventKey="rol">
-                            <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillPersonLinesFill /></i></NavIcon>
-                            <NavText style={{ fontSize: "20px" }}>Roles</NavText>
-                        </NavItem>
+                    <NavItem eventKey="users">
+                        <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillPeopleFill /></i></NavIcon>
+                        <NavText style={{ fontSize: "20px" }}>Usuarios</NavText>
+                    </NavItem>
 
-                        <NavItem eventKey="facturaControl">
-                            <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillFileEarmarkBarGraphFill /></i></NavIcon>
-                            <NavText style={{ fontSize: "20px" }}>Facturas</NavText>
-                        </NavItem>
+                    <NavItem eventKey="rol">
+                        <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillPersonLinesFill /></i></NavIcon>
+                        <NavText style={{ fontSize: "20px" }}>Roles</NavText>
+                    </NavItem>
 
-                        <NavItem eventKey="contact">
-                            <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillEnvelopeFill /></i></NavIcon>
-                            <NavText style={{ fontSize: "20px" }}>Contactanos</NavText>
-                        </NavItem>
-                        
-                    </SideNav.Nav>
+                    <NavItem eventKey="facturaControl">
+                        <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillFileEarmarkBarGraphFill /></i></NavIcon>
+                        <NavText style={{ fontSize: "20px" }}>Facturas</NavText>
+                    </NavItem>
+
+                    <NavItem eventKey="contact">
+                        <NavIcon><i style={{ fontSize: "1.5em" }}><BsFillEnvelopeFill /></i></NavIcon>
+                        <NavText style={{ fontSize: "20px" }}>Contactanos</NavText>
+                    </NavItem>
+
+                </SideNav.Nav>
 
             </SideNav>
         </div >
