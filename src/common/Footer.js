03c1de40473605d8f36f43340f070c64d4040ad1
yahirdev13 <yahir.dev13@gmail.com>
2023-06-28 10:44:06 -0600

    actualizacion de la pagina de preguntas frecuentes

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -5,7 +5,6 @@ export default function Footer() {
     return (
         <footer>
             <div class="pie-pagina">
-
                 <div class="grupo-2">
                     <small>&copy; 2023 <b>Jardines de México</b> - Todos los Derechos Reservados.</small>
                 </div>
