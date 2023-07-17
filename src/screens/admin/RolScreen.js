7feb1dcfeb2fb333389527549329df919490fcbf
yahirdev13 <yahir.dev13@gmail.com>
2023-07-17 17:56:20 -0600

    cambios pantalla de roles

--- src/screens/admin/RolScreen.js
+++ src/screens/admin/RolScreen.js
@@ -1,63 +1,184 @@
 import React from 'react'
 import Menu from '../../common/Admin/Menu'
+import './style.css'
+
+import DataTable from 'react-data-table-component'
 
 
 //gif
 import advertencia from '../../gifs/alerta.gif'
 
-export default function RolScreen() {
-  return (
-    <div>
-      <Menu />
 
-      <div style={{ paddingLeft: "8%", paddingRight: "5%" }}>
-        <h1 class="mt-5">Control de Roles</h1>
-        <div style={{ left: "250px" }} className='container-fluid'>
-          <table class="table " style={{ width: "auto" }}>
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Rol</th>
-                <th scope="col">Acciones</th>
-              </tr>
-            </thead>
-            <tbody class="table-group-divider">
-              <tr style={{ verticalAlign: "middle" }}>
-                <th scope="row ">1</th>
-                <td>Administrador</td>
-                <td>
-                  <button type="button" class="btn btn-primary " style={{ marginRight: "10px" }} data-bs-toggle="modal" data-bs-target="#staticBackdrop">Detalles</button>
-                  <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#eliminarRol">Eliminar</button>
-                </td>
-              </tr>
-              <tr style={{ verticalAlign: "middle" }}>
-                <th scope="row ">2</th>
-                <td>Gerente</td>
-                <td>
-                  <button type="button" class="btn btn-primary " style={{ marginRight: "10px" }}>Detalles</button>
-                  <button type="button" class="btn btn-danger " data-bs-toggle="modal" data-bs-target="#eliminarRol">Eliminar</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+const data = [
+  { id: 1, rol: 'Administrador', descripcion: 'Tiene acceso a todas las funciones del sistema' },
+]
+
+
+const columnas = [
+  {
+    name: '#',
+    selector: 'id',
+    sortable: true,
+    width: '80px',
+  },
+  {
+    name: 'Nombre del rol',
+    selector: 'rol',
+    sortable: true
+  },
+  {
+    name: 'Descripción',
+    selector: 'descripcion',
+    sortable: true
+  },
+  {
+    name: 'Acciones',
+    width: '230px',
+    cell: row => (
+      <div>
+        <button type="button" class="btn btn-primary me-2 mb-2 mt-1" data-bs-toggle="modal" data-bs-target="#editarRol" >Editar <i class="bi bi-pencil-fill"></i></button>
+        <button type="button" class="btn btn-danger mb-2 mt-1" data-bs-toggle="modal" data-bs-target="#eliminarRol" >Eliminar <i class="bi bi-trash-fill"></i></button>
+      </div >
+    ),
+    ignoreRowClick: true,
+    allowOverflow: true,
+    button: true,
+  },
+]
+
+//define la configuracion de la paginacion de la tabla
+
+const paginacionOpciones = {
+  rowsPerPageText: 'Filas por Página',
+  rangeSeparatorText: 'de',
+  selectAllRowsItem: true,
+  selectAllRowsItemText: 'Todos'
+}
+
+export default function RolScreen() {
+
+  return (
+    <div className='component'>
+      <Menu />
+      <div>
+        <div class="mx-auto p-2">
+          <h2>Control de roles</h2>
+        </div>
+        <div class="d-flex justify-content-end pt-3">
+          <button type="button" class="btn btn-primary crearRol" data-bs-toggle="modal" data-bs-target="#crearRol">Agregar rol <i class="bi bi-person-fill-add"></i></button>
+        </div>
+
+      </div>
+      <div class="card mt-3">
+        <div class="card-body">
+          <div class="d-flex justify-content-end mb-2">
+            <input type='text' placeholder='Buscar...' class='form-control me-2' style={{ width: "300px" }}
+            // onChange={this.onChange}
+            />
+
+          </div>
+          <div class="table-responsive">
+            <DataTable
+              columns={columnas}
+              data={data}
+              title="Lista de roles"
+              pagination
+              highlightOnHover
+              paginationComponentOptions={paginacionOpciones}
+              fixedHeader
+              fixedHeaderScrollHeight="auto"
+              noDataComponent="No se encontró ningún rol"
+            />
+          </div>
+
         </div>
       </div>
 
-      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      {/* modal para la creacion de rol */}
+      <div class="modal fade" id="crearRol" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalles de Rol: </h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">Agregar nuevo rol</h1>
             </div>
             <div class="modal-body">
-              <p class="placeholder-glow">
-                <span class="placeholder col-12">aqui va el texto jejejej</span>
-              </p>
+              <form>
+                <div class="mb-3">
+                  <label class="form-label">Nombre del rol</label>
+                  <input type="text" class="form-control" placeholder="Nombre del rol" required />
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Descripción</label>
+                  <textarea type="text-area" class="form-control" placeholder="Ingrese una descripción breve" style={{ height: "100px" }} required />
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Permisos</label>
+                  <table class="table table-borderless align-middle">
+                    <thead>
+                      <tr>
+                        <th scope="col">Módulo</th>
+                        <th scope="col" class="text-center">Ver</th>
+                        <th scope="col" class="text-center">Crear</th>
+                        <th scope="col" class="text-center">Editar</th>
+                        <th scope="col" class="text-center">Eliminar</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">Usuarios</th>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Roles</th>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Mensajes</th>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" disabled /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" disabled /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Facturas</th>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" disabled /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" disabled /></td>
+                        <td class="text-center"><input type="checkbox" class="form-check-input" /></td>
+                      </tr>
+                    </tbody>
+
+                  </table>
+                </div>
+              </form>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-              <button type="button" class="btn btn-primary">Holis</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Guardar nuevo rol</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* modal para la edicion de rol */}
+      <div class="modal fade" id="editarRol" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">Editar Rol</h1>
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Guardar Cambios</button>
             </div>
           </div>
         </div>
